a77a9c957a99ba342d25d9f8bfcb6604e527b033
Doug Loomer <loomerds@gmail.com>
2019-05-12 18:29:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(line, name) {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+
+function nowServing(line) {
+  let next = "There is nobody waiting to be served!"
+  if(line.length>0) {
+    next = line[0];
+    line.shift();
+  }
+  return next;
+}
